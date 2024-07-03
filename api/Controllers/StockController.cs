efa8fd773234f3da565124d21710b84c244ec412
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2024-07-02 21:36:10 -0600

    changed the controller methods to be asynchronous

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
 using api.Mappers;
+using api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using Microsoft.JSInterop.Infrastructure;
 
@@ -21,18 +23,20 @@ namespace api.Controllers
             _context = context;
         }
         //READ
+        #region GET
         [HttpGet]
-        public IActionResult Getll()
+        public async Task<IActionResult> Getll()
         {
-            var stocks = _context.Stocks.ToList().Select(s=> s.ToStockDto());//defered execution - this is so that it selects certain information you want to show
+            var stocks = await _context.Stocks.ToListAsync();
+            var stockDto = stocks.Select(s=> s.ToStockDto());//defered execution - this is so that it selects certain information you want to show
             //==> Select(s=> s.ToStockDto()) ==> This is from the Mappers Method  -
             //Make the DTo first then the mapper
             return Ok(stocks);
         }
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var stock = _context.Stocks.Find(id); //find the stock by id
+            var stock = await _context.Stocks.FindAsync(id); //find the stock by id
 
             if(stock ==null) //if cant find it then return not found
             {
@@ -40,15 +44,15 @@ namespace api.Controllers
             }
             return Ok(stock.ToStockDto()); //else return the stock by Id
         }
-
+        #endregion
 
         #region POST
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto createStockDto)
         {
-            var stockModel = createStockDto.ToStockFromCreateDto(); //making based on the dto
-            _context.Stocks.Add(stockModel);
-            _context.SaveChanges();
+            var stockModel =  createStockDto.ToStockFromCreateDto(); //making based on the dto
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
             //it is going to execute the GetById method
             //it is going to pass in this new object into the id
             //after that it is going to return in a form of ToStockDto
@@ -61,16 +65,16 @@ namespace api.Controllers
 
         [HttpPut]
         [Route("{id}")] //specify our search based on id
-        public IActionResult Update([FromRoute]int id, [FromBody]UpdateStockRequestDto updateStockDto )
+        public async Task< IActionResult> Update([FromRoute]int id, [FromBody]UpdateStockRequestDto updateStockDto )
         {
-            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
             if(stockModel == null)
                 return NotFound();
 
                 stockModel.Symbol = updateStockDto.Symbol;
                 stockModel.CompanyName = updateStockDto.CompanyName;
 
-            _context.SaveChanges(); //this is whats actually going to send to the database
+           await _context.SaveChangesAsync(); //this is whats actually going to send to the database
 
             return Ok(stockModel.ToStockDto());
 
@@ -81,15 +85,15 @@ namespace api.Controllers
         #region DELETE
         [HttpDelete]
         [Route("{id}")]
-        public IActionResult Delete([FromRoute]int id)
+        public async Task <IActionResult> Delete([FromRoute]int id)
         {
-            var stockModel = _context.Stocks.FirstOrDefault(x=>x.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x=>x.Id == id);
 
             if(stockModel == null)
             return NotFound();
 
-            _context.Stocks.Remove(stockModel);
-            _context.SaveChanges();
+             _context.Stocks.Remove(stockModel); //with delete the remove is not an asynchronous funtion
+           await _context.SaveChangesAsync();
                 
             return Ok(stockModel.ToStockDto());
         }
