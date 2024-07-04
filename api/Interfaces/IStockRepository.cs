6a4e04c1332b9c2649be14d748bd30e965d54e12
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2024-07-03 20:31:37 -0600

    Added more objects to our IStockRepository

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
+using api.Dtos.Stock;
 using api.Models;
 
 namespace api.Interfaces
@@ -10,6 +12,11 @@ namespace api.Interfaces
     //& allow us to abstract our code away    
     public interface IStockRepository
     {
+        //remember Task == object return type
          Task<List<Stock>> GetAllAsync();
+         Task<Stock?> GetByIdAsync(int id);//FirstOrDefault CAN BE NULL - so we need this ? in our Stock
+         Task<Stock> CreateAsync(Stock stockModel);// this will create our entity model - Stock
+         Task <Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+         Task<Stock?> DeleteAsync(int id);
     }
 }
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,26 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace api.Repository
 {
     public class StockRepository : IStockRepository
-    
+
     {
+        //REPOSITORY EXIST FOR DATABASE CALLS
+
 
         //bring in our context
+        private readonly ApplicationDBContext _context;
         
         public StockRepository(ApplicationDBContext context)
         {
             _context = context;
         }
+
+        public Task<Stock> CreateAsync(Stock stockModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<List<Stock>> GetAllAsync()
         {
             return await _context.Stocks.ToListAsync();
         }
-        
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
